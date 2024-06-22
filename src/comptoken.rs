f5898c480fe462325622d910107b12573be6d7a8
connor-compto <connor@compto.com>
2024-06-22 11:50:46 -0500

    switch to fully autogenerated, ignored file

--- src/comptoken.rs
+++ src/comptoken.rs
@@ -17,24 +17,8 @@ use spl_token::instruction::mint_to;
 // declare and export the program's entrypoint
 entrypoint!(process_instruction);
 
-// vvv This line is automatically updated by full_deploy_test.py.
-// static COMPTOKEN_ADDRESS: Pubkey = pubkey!("42mrBXsurtZmiyKu6JpchUvhukjJKeUD2QNUA5TAbyjg");
-// ^^^ DO NOT TOUCH. ^^^
-
-// A given seed and program id have a 50% chance of creating a valid PDA.
-// Before building/deploying, we find the canonical seed by running 
-//      `solana find-program-derived-address <program_id>`
-// This is an efficiency optimization. We are using a static seed to create the PDA with no bump.
-// We ensure when deploying that the program id is one that only needs the seed above and no bump.
-// This is because 
-//      (1) create_program_address is not safe if using a user provided bump.
-//      (2) find_program_address is expensive and we want to avoid iterations.
-// vvv This line is automatically updated by full_deploy_test.py.
-// static COMPTO_STATIC_ADDRESS_SEED: u8 = 255;
-// ^^^ DO NOT TOUCH. ^^^
 mod comptoken_generated;
-use comptoken_generated::COMPTOKEN_ADDRESS;
-use comptoken_generated::COMPTO_STATIC_ADDRESS_SEED;
+use comptoken_generated::{COMPTOKEN_ADDRESS, COMPTO_STATIC_ADDRESS_SEED};
 
 
 // #[derive(Debug, Default, BorshDeserialize, BorshSerialize)]
