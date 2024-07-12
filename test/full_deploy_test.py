25e03ece565a0e8262dcc4a919d35875135f68eb
connor-compto <connor@compto.com>
2024-07-11 20:23:29 -0500

    fix first run issue with generated directory not existing

--- test/full_deploy_test.py
+++ test/full_deploy_test.py
@@ -10,12 +10,13 @@ from typing import Any, Self, Type
 TEST_PATH = Path(__file__).parent
 PROJECT_PATH = TEST_PATH.parent
 CACHE_PATH = TEST_PATH / ".cache"
+GENERATED_PATH = PROJECT_PATH / "src/generated"
 COMPTO_MD5_JSON = CACHE_PATH / "compto_md5sum.json"
 COMPTO_PROGRAM_ID_JSON = CACHE_PATH / "compto_program_id.json"
 COMPTO_SO = PROJECT_PATH / "target/deploy/comptoken.so"
 COMPTOKEN_MINT_JSON = CACHE_PATH / "comptoken_mint.json"
 TEST_USER_ACCOUNT_JSON = CACHE_PATH / "test_user_account.json"
-COMPTO_GENERATED_RS_FILE = PROJECT_PATH / "src/generated/comptoken_generated.rs"
+COMPTO_GENERATED_RS_FILE = GENERATED_PATH / "comptoken_generated.rs"
 COMPTO_GLOBAL_DATA_ACCOUNT_JSON = CACHE_PATH / "compto_global_data_account.json"
 COMPTO_INTEREST_BANK_ACCOUNT_JSON = CACHE_PATH / "compto_interest_bank_account.json"
 COMPTO_UBI_BANK_ACCOUNT_JSON = CACHE_PATH / "compto_ubi_bank_account.json"
@@ -288,6 +289,7 @@ def waitTillValidatorReady(validator: BackgroundProcess):
 if __name__ == "__main__":
     # create cache if it doesn't exist
     run(f"[ -d {CACHE_PATH} ] || mkdir {CACHE_PATH} ")
+    run(f"[ -d {GENERATED_PATH} ] || mkdir {GENERATED_PATH} ")
     # If ProgramId doesn't exist, we need to build WITHOUT the testmode feature.
     # This is because the static seed in testmode depends on ProgramId and ProgramId
     # is generated on the first build.
