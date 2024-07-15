b1bd692fcd3ff63f928d9cef857f20b40c2fe781
connor-compto <connor@compto.com>
2024-07-15 14:36:17 -0500

    add calculate_distribution_limiter_basis_points

--- src/constants.rs
+++ src/constants.rs
@@ -1,5 +1,9 @@
 // TODO: Explain where this number comes from
-pub const MAX_DAILY_COMPTOKEN_DISTRIBUTION_BASIS_PTS: u64 = 75;
-pub const BASIS_PTS_DIVISOR: u64 = 10000;
+pub const MAX_DAILY_COMPTOKEN_DISTRIBUTION_BASIS_PTS: u64 = 750;
+pub const BASIS_PTS_DIVISOR: u64 = 100_000;
 // TODO: This number deserves scrutiny and justification.
-pub const COMPTOKEN_DISTRIBUTION_MULTIPLIER: u64 = 146000;
+pub const COMPTOKEN_DISTRIBUTION_MULTIPLIER: u64 = 146_000;
+
+pub const MIN_SUPPLY_LIMIT_AMT: u64 = 1_000_000;
+pub const ADJUST_FACTOR: f64 = 0.3;
+pub const END_GOAL_PERCENT_INCREASE: u64 = 61;
