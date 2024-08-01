189c1a5e532a87e04713c9f4a095e0fb1bc4b806
connor-compto <connor@compto.com>
2024-08-01 16:52:02 -0500

    switch comptoken to using verify_accounts_util

--- comptoken-utils/src/verify_accounts.rs
+++ comptoken-utils/src/verify_accounts.rs
@@ -11,7 +11,9 @@ impl<'a> VerifiedAccountInfo<'a> {
         Self(account)
     }
 
-    fn verify_account_signer_or_writable(account: &AccountInfo<'a>, needs_signer: bool, needs_writable: bool) -> Self {
+    pub fn verify_account_signer_or_writable(
+        account: &AccountInfo<'a>, needs_signer: bool, needs_writable: bool,
+    ) -> Self {
         // only panic if signing/writing is needed and the account does not meet the requirements
         assert!(!needs_signer || account.is_signer);
         assert!(!needs_writable || account.is_writable);
@@ -26,6 +28,14 @@ impl<'a> VerifiedAccountInfo<'a> {
         (Self::verify_account_signer_or_writable(account, needs_signer, needs_writable), bump)
     }
 
+    pub fn verify_pda_with_bump(
+        account: &AccountInfo<'a>, program_id: &Pubkey, seeds: &[&[u8]], needs_signer: bool, needs_writable: bool,
+    ) -> Self {
+        let result = Pubkey::create_program_address(seeds, program_id).expect("correct bump");
+        assert_eq!(*account.key, result);
+        Self::verify_account_signer_or_writable(account, needs_signer, needs_writable)
+    }
+
     pub fn verify_sysvar<S: SysvarId>(account: &AccountInfo<'a>) -> Self {
         assert!(S::check_id(account.key));
         Self::new(account.clone())
@@ -35,7 +45,7 @@ impl<'a> VerifiedAccountInfo<'a> {
         Self::verify_account_signer_or_writable(account, true, true)
     }
 
-    pub fn verify_specific_account(
+    pub fn verify_specific_address(
         account: &AccountInfo<'a>, address: &Pubkey, needs_signer: bool, needs_writable: bool,
     ) -> Self {
         assert_eq!(account.key, address);
